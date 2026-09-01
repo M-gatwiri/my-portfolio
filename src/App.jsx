64048ae61dc67236b7d14421c804047279f4dcdb
M-gatwiri <mgatwiri806@gmail.com>
2026-09-01 21:55:59 +0300

    Add new project features

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import LayOut from './pages/LayOut'
 import Home from './pages/Home'
